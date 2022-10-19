0e479e304e30a53cf66ecf13909620a11d1504fe
Jonathan <jhonp0991@gmail.com>
2022-10-19 12:11:46 -0500

    modificacion motorbike

--- src/main/java/com/misiontic/masterclass3/entities/Motorbike.java
+++ src/main/java/com/misiontic/masterclass3/entities/Motorbike.java
@@ -24,81 +24,60 @@ public class Motorbike implements Serializable {
   private String description;
 
   @ManyToOne
-  @JoinColumn(name = "category")
-  @JsonIgnoreProperties("products")
+  @JoinColumn(name = "categoryId")
+  @JsonIgnoreProperties("motorbikes")
   private Category category;
 
-  @ManyToOne
-  @JoinColumn(name = "messages")
-  @JsonIgnoreProperties("products")
   private Message messages;
-
-  @ManyToOne
-  @JoinColumn(name = "reservations")
-  @JsonIgnoreProperties("products")
   private Reservation reservations;
-
   public Integer getId() {
     return id;
   }
-
+  public void setId(Integer id) {
+    this.id = id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getBrand() {
+    return brand;
+  }
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+  public Integer getYear() {
+    return year;
+  }
+  public void setYear(Integer year) {
+    this.year = year;
+  }
+  public String getDescription() {
+    return description;
+  }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  public Category getCategory() {
+    return category;
+  }
+  public void setCategory(Category category) {
+    this.category = category;
+  }
   public Message getMessages() {
     return messages;
   }
-
   public void setMessages(Message messages) {
     this.messages = messages;
   }
-
   public Reservation getReservations() {
     return reservations;
   }
-
   public void setReservations(Reservation reservations) {
     this.reservations = reservations;
   }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public Integer getYear() {
-    return year;
-  }
-
-  public void setYear(Integer year) {
-    this.year = year;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getBrand() {
-    return brand;
-  }
-
-  public void setBrand(String brand) {
-    this.brand = brand;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public Category getCategory() {
-    return category;
-  }
-
-  public void setCategory(Category category) {
-    this.category = category;
-  }
+  
 }
